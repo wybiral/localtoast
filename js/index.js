1ca6ea8bec6ea3030be52abf13f2a7aba1e3cd45
davy <davy.wybiral@gmail.com>
2019-05-31 13:56:37 -0500

    service discovery

--- js/index.js
+++ js/index.js
@@ -1,31 +1,79 @@
-const ports = [
-    [80,    'HTTP'],
-    [631,   'Printing'],
-    [3306,  'MySQL'],
-    [3690,  'SVN'],
-    [6006,  'TensorBoard'],
-    [6379,  'Redis'],
-    [8000,  'HTTP'],
-    [8080,  'HTTP'],
-    [8888,  'HTTP'],
-    [9050,  'Tor Controller'],
-    [9051,  'Tor SOCKS Proxy'],
-    [9150,  'Tor Browser Controller'],
-    [9151,  'Tor Browser SOCKS Proxy'],
-    [9200,  'ElasticSearch'],
-    [16423, 'Keybase'],
-    [27017, 'MongoDB'],
-    [32400, 'Plex'],
+const services = [
+    {
+        url: 'http://127.0.0.1:80',
+        name: 'HTTP (80)',
+    },
+    {
+        url: 'http://127.0.0.1:631',
+        name: 'Printing (631)',
+    },
+    {
+        url: 'http://127.0.0.1:3000/img/gitea-lg.png',
+        name: 'Gitea (3000)',
+    },
+    {
+        url: 'http://127.0.0.1:3306',
+        name: 'MySQL (3306)',
+    },
+    {
+        url: 'http://127.0.0.1:3690',
+        name: 'SVN (3690)',
+    },
+    {
+        url: 'http://127.0.0.1:6006/data/environment',
+        name: 'TensorBoard (6006)',
+    },
+    {
+        url: 'http://127.0.0.1:6379',
+        name: 'Redis (6379)',
+    },
+    {
+        url: 'http://127.0.0.1:8000',
+        name: 'HTTP (8000)',
+    },
+    {
+        url: 'http://127.0.0.1:8080',
+        name: 'HTTP (8080)',
+    },
+    {
+        url: 'http://127.0.0.1:8888',
+        name: 'HTTP (8888)',
+    },
+    {
+        url: 'http://127.0.0.1:9050',
+        name: 'Tor Controller (9050)',
+    },
+    {
+        url: 'http://127.0.0.1:9051',
+        name: 'Tor SOCKS Proxy (9051)',
+    },
+    {
+        url: 'http://127.0.0.1:9150',
+        name: 'Tor Browser Controller (9150)',
+    },
+    {
+        url: 'http://127.0.0.1:9151',
+        name: 'Tor Browser SOCKS Proxy (9151)',
+    },
+    {
+        url: 'http://127.0.0.1:9200/_cat',
+        name: 'ElasticSearch (9200)',
+    },
+    {
+        url: 'http://127.0.0.1:16423',
+        name: 'Keybase (16423)',
+    },
+    {
+        url: 'http://127.0.0.1:27017',
+        name: 'MongoDB (27017)',
+    },
+    {
+        url: 'http://127.0.0.1:32400',
+        name: 'Plex (32400)',
+    },
 ];
 
-// Array of [address, description] pairs to be scanned
-const scanlist = [];
-
 window.onload = () => {
-    // Create scanlist
-    addHost('127.0.0.1');
-    // Shake it up
-    shuffle(scanlist);
     // Start workers
     for (let i = 0; i < 10; i++) {
         setTimeout(worker, 0);
@@ -33,35 +81,18 @@ window.onload = () => {
 };
 
 function worker() {
-    if (scanlist.length == 0) {
+    if (services.length == 0) {
+        document.querySelector('h1').innerText = 'Results:';
         return;
     }
-    const x = scanlist.pop();
-    const addr = x[0];
-    fetch(addr, {mode: 'no-cors'}).then(resp => {
+    const x = services.shift();
+    fetch(x.url, {mode: 'no-cors', cache: 'no-cache'}).then(resp => {
         const el = document.createElement('div');
-        el.innerText = 'Found: ' + x[0] + ' (' + x[1] + ')';
+        el.innerText = 'Found: ' + x.name;
         document.body.appendChild(el);
         setTimeout(worker, 0);
     }).catch(err => {
         // ¯\_(ツ)_/¯
         setTimeout(worker, 0);
     });
-}
-
-// Add all known ports to scanlist for host
-function addHost(host) {
-    ports.forEach(x => {
-        scanlist.push(['http://' + host + ':' + x[0], x[1]]);
-    });
-}
-
-// Shuffle array
-function shuffle(a) {
-    for (let i = a.length - 1; i > 0; i--) {
-        const j = 0 | Math.random() * (i + 1);
-        const x = a[i];
-        a[i] = a[j];
-        a[j] = x;
-    }
 }
